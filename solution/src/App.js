a25a722b7ea559a7d62db49bebd6fc777ac35d73
furkan <furkanesen35@gmail.com>
2022-06-29 19:34:13 +0200

    header part done

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <h1>Language Card Project</h1>
+      <Header/>
     </div>
   );
 }
